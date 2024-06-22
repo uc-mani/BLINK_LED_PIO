01b92382ae35d8364b5b3a3dea36a1dd2aab047b
Usman C <110965555+uc-mani@users.noreply.github.com>
2024-06-22 22:00:00 +0500

    Update main.cpp

--- BLINK_LED_PIO/src/main.cpp
+++ BLINK_LED_PIO/src/main.cpp
@@ -1,21 +1,21 @@
 #include <Arduino.h>
 
 
-//#define LED_PIN 22
+#define LED_PIN 22
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.println(touchRead(4));
   if(touchRead(4) < 50 ){
-    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(LED_PIN, LOW);
   }
   else{
-    digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(LED_PIN, HIGH);
   }
   delay(100);
 }
